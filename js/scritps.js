bc4fff8dcbc7dba600be625469aa35511804edc7
Darkpfeffer <darkpfeffer97@gmail.com>
2023-02-09 19:52:23 +0100

    strike element on double click

--- js/scritps.js
+++ js/scritps.js
@@ -1,10 +1,19 @@
 function newItem() {
-// Adding a new item to the list
+    // Adding a new item to the list
+    let li= $('<li></li>');
     let inputValue= $('#input').val();
+    li.append(inputValue);
 
     if ($('#input').val()=== '') {
         window.alert("You must write something!");
     } else {
-        $('#list').append('<li>'+ inputValue+ '</li>');
+        $('#list').append(li)
     }
+
+    // Crossing out an item from the list
+    function crossOut() {
+        li.toggleClass('strike');
+    }
+
+    li.on('dblclick', crossOut);
 }
