f4f56e0b62e5127e9a9c20fe10f32bb16f9a02c8
Darkpfeffer <darkpfeffer97@gmail.com>
2023-02-09 17:23:33 +0100

    add a new element to the list

--- js/scritps.js
+++ js/scritps.js
@@ -1,2 +1,10 @@
-let li= $('#list').append('<li></li>');
-let inputValue= $('#input').attr('value');
+function newItem() {
+// Adding a new item to the list
+    let inputValue= $('#input').val();
+
+    if ($('#input').val()=== '') {
+        window.alert("You must write something!");
+    } else {
+        $('#list').append('<li>'+ inputValue+ '</li>');
+    }
+}
