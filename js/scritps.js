04c8db4fe39f45b5d1c3a5b212bcedc9fbd4042e
Darkpfeffer <darkpfeffer97@gmail.com>
2023-02-09 16:55:45 +0100

    Undo previous changes

--- js/scritps.js
+++ js/scritps.js
@@ -1,2 +1,19 @@
-let li= $('#list').append('<li></li>');
-let inputValue= $('#input').attr('value');
+function newItem(){
+
+    // Adding new item to the list
+    let inputValue= $('#input').attr('value');
+
+    if (inputValue=== '') {
+        alert('You must write something!');
+    } else {
+        $('#list').append('<li></li>').text(inputValue);
+        console.log(inputValue)
+    }
+
+    //Crossing out an item from the list
+    function crossOut() {
+        $('.active').toggleClass('strike')
+    }
+
+    $('li').on('dblclick', crossOut); 
+}
