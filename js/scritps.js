609fd401ec25063141027bfaae2e1906689dd4cc
Darkpfeffer <darkpfeffer97@gmail.com>
2023-02-09 16:56:08 +0100

    Revert "Undo previous changes"
    This reverts commit 04c8db4fe39f45b5d1c3a5b212bcedc9fbd4042e.

--- js/scritps.js
+++ js/scritps.js
@@ -1,19 +1,2 @@
-function newItem(){
-
-    // Adding new item to the list
-    let inputValue= $('#input').attr('value');
-
-    if (inputValue=== '') {
-        alert('You must write something!');
-    } else {
-        $('#list').append('<li></li>').text(inputValue);
-        console.log(inputValue)
-    }
-
-    //Crossing out an item from the list
-    function crossOut() {
-        $('.active').toggleClass('strike')
-    }
-
-    $('li').on('dblclick', crossOut); 
-}
+let li= $('#list').append('<li></li>');
+let inputValue= $('#input').attr('value');
